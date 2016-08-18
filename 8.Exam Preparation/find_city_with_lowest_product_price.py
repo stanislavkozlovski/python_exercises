5fb6642d402acb6c35a8f1a2e47b71bce1a042bb
Enether <familyguyuser192@windowslive.com>
2016-08-18 17:32:30 +0300

    Made the solution more PYTHONIC to find_city_with_lowest_product_price.py from 8.Exam Preparation

--- 8.Exam Preparation/find_city_with_lowest_product_price.py
+++ 8.Exam Preparation/find_city_with_lowest_product_price.py
@@ -1,26 +1,23 @@
+"""
+Given a product's ID and a sales text document. Find the city from which the item has been sold for the lowest price.
+
+Sample input:
+G17628
+1-sales.txt
+"""
 import csv
-from sys import float_info
 
 INDEX_SALES_PRODUCT_ID = 0
 INDEX_SALES_CITY = 2
 INDEX_SALES_PRICE = 4
 
-placeholder = (float_info.max, 0)   # type: tuple for easy indexing
 target_product_id = input()
 sales_file_name = input()
 
-lowest_price_dict = {}  # key: product_id, value: tuple (lowest_price: float, city_name: str)
 
 with open(sales_file_name, encoding='utf-8') as _:
     sf_reader = csv.reader(_)
-
-    for line in sf_reader:
-        if line:
-            product_id = line[INDEX_SALES_PRODUCT_ID]
-            city_name = line[INDEX_SALES_CITY]
-            price = float(line[INDEX_SALES_PRICE])
-
-            # TODO: check <=
-            if price < lowest_price_dict.get(product_id, placeholder)[0]:
-                lowest_price_dict[product_id] = (price, city_name)
-print(lowest_price_dict[target_product_id][1])
+    # get a list of tuples with sales only for the product we're searching for
+    sales_filtered = filter(lambda sale: sale[INDEX_SALES_PRODUCT_ID] == target_product_id, sf_reader)
+    # get the sale with the minimum price and print the city from it
+    print(min(sales_filtered, key=lambda x: x[INDEX_SALES_PRICE])[INDEX_SALES_CITY])
