9c89f8ff0bd749ed041954ec9dc4280fe446d32f
Enether <familyguyuser192@windowslive.com>
2016-12-05 19:46:57 +0200

    Added some warm-up hackerrank algorithm problems

---
+++ hackerrank/algorithms/warmup/a_very_big_sum.py
@@ -0,0 +1,15 @@
+"""
+You are given an array of integers of size N.
+You need to print the sum of the elements in the array, keeping in mind that some of those integers may be quite large.
+
+Input Format
+The first line of the input consists of an integer N.
+The next line contains N space-separated integers contained in the array.
+
+Output Format
+Print a single value equal to the sum of the elements in the array.
+"""
+
+
+input()  # unneeded
+print(sum([int(num) for num in input().split()]))
