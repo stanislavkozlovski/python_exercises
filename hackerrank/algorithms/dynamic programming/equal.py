986463833e2730db2d5dbf1d68145935bd7a3c7d
Enether <familyguyuser192@windowslive.com>
2017-04-09 23:53:14 +0300

    Improved Hackerrank/DP/Equal's logic a bit

--- hackerrank/algorithms/dynamic programming/equal.py
+++ hackerrank/algorithms/dynamic programming/equal.py
@@ -1,39 +1,37 @@
 from copy import deepcopy
 
 
-def get_number_of_operations_to_reduce(array, idx, wanted_number):
+def get_number_of_operations_to_reduce(number, wanted_number):
     """
     Given a number at an index of an array and a wanted number, return the number of operations that are needed
     to reduce the array number to the wanted number by only removing 5, 2 or 1 per operation
     ex: arr = [1,2,3,4], idx = 3, wanted_number = 1: number at arr[idx] is 4. 4 can be reduced to 1 with 2 operations:
         4-2=2 2-1=1
     """
-    number = array[idx]
     diff = number - wanted_number
     needed_operations = 0
 
     while diff != 0:
         needed_operations += 1
         if diff >= 5:
-            array[idx] -= 5
+            number -= 5
         elif diff >= 2:
-            array[idx] -= 2
+            number -= 2
         elif diff > 0:
-            array[idx] -= 1
-        diff = array[idx] - wanted_number
+            number -= 1
+        diff = number - wanted_number
 
     return needed_operations
 
-
-def minimize_chocolates(chocolates, minimize_to_zero=False):
+import sys
+def minimize_chocolates(chocolates, subtract_from_min=0):
     """
     Get the total number of operations needed to reduce the whole array to a certain value `min_chocolate`
-    :param minimize_to_zero: A boolean indicating if we want to reduce the array to 0s
+    :param subtract_from_min: A number we want to subtract from our min value (see main() docstring)
     """
-    if minimize_to_zero:
-        min_chocolate = 0
-    else:
-        min_chocolate = min(chocolates)
+    min_chocolate = min(chocolates) - subtract_from_min
+    if min_chocolate < 0:
+        return sys.maxsize
     operations_needed = {}  # holds a key of the number and the operations needed for it to reach min_chocolate
     overall_operations = 0
 
@@ -41,11 +39,10 @@ def minimize_chocolates(chocolates, minimize_to_zero=False):
         if choc != min_chocolate:
             if choc not in operations_needed:
                 # calculate the number of needed operations to reach the number
-                current_operations = get_number_of_operations_to_reduce(chocolates, idx, min_chocolate)
+                current_operations = get_number_of_operations_to_reduce(chocolates[idx], min_chocolate)
                 overall_operations += current_operations
                 operations_needed[choc] = current_operations
             else:
-                chocolates[idx] = min_chocolate
                 overall_operations += operations_needed[choc]
     return overall_operations
 
@@ -58,17 +55,23 @@ def main():
     We have 2 main strategies.
     1 - Get the minimum amount of chocolate in the chocolates and reduce every other chocolate to that amount
     2 - Reduce every chocolate to 0
+    3 - Reduce the minimum ammount by some and reduce all others to that
     In most cases, 1 is more optimal, but in some strange edge cases like - [2,5,5,5,5,5] - reducing to 0 is better
+    But there are also edge cases like - 6 10 10 10 10 10, where reducing 6 to 5 and all 10s to 5 is the better approach
     """
     test_cases = int(input())
     for _ in range(test_cases):
         _ = input()
         chocolates = [int(part) for part in input().split()]
 
-        result_a = minimize_chocolates(deepcopy(chocolates))
-        result_b = minimize_chocolates(chocolates, minimize_to_zero=True)
+        result_a = minimize_chocolates(chocolates)
+        result_b = minimize_chocolates(chocolates, subtract_from_min=min(chocolates))
+        result_c = minimize_chocolates(chocolates, 1)
+        result_d = minimize_chocolates(chocolates, 2)
+        result_e = minimize_chocolates(chocolates, 5)
 
-        print(min(result_a, result_b))
+
+        print(min(min(min(min(result_a, result_b), result_c), result_d), result_e))
 
 
 if __name__ == '__main__':
