6c8e75d692c92f15d42e71cbf6a9f8da6bf802cb
Stanislav Kozlovski <familyguyuser192@windowslive.com>
2019-04-17 14:49:34 +0100

    Extend topological sort to detect cycles

--- random_interview_prep/topological_sort.py
+++ random_interview_prep/topological_sort.py
@@ -3,22 +3,33 @@ graph = {
     4: [0, 1],
     2: [3],
     5: [2, 0],
-    1: [],
+    1: [],  # add 4 here to create a cycle
     3: [1],
     0: []
 }
 
+other_graph = {
+    4: [3, 2],
+    3: [2, 1],
+    2: [1],  # add 3 here to create a cycle
+    1: []
+}
+
 tp_sorted_nodes = []
 visited = set()
+visited_this_iter = set()
 
 
 def traverse_nodes(curr_node: int, graph: dict):
     """ Use DFS to traverse through the nodes """
-    global tp_sorted_nodes, visited
+    global tp_sorted_nodes, visited, visited_this_iter
     if curr_node in visited:
         return
     visited.add(curr_node)
+    visited_this_iter.add(curr_node)
     for child in graph[curr_node]:
+        if child in visited_this_iter and child not in tp_sorted_nodes:
+            raise Exception('cycle!')
         traverse_nodes(child, graph)
     tp_sorted_nodes.append(curr_node)
 
@@ -43,6 +54,12 @@ def traverse_nodes_iterative(curr_node: int, graph: dict):
     while len(topologically_visited_nodes) != 0:
         tp_sorted_nodes.append(topologically_visited_nodes.pop())
 
-for node in graph.keys():
-    traverse_nodes_iterative(node, graph)
-print(f'Topological sorting is {tp_sorted_nodes}')
+
+def topological_sort(graph: dict):
+    global visited_this_iter, tp_sorted_nodes
+    for node in graph.keys():
+        traverse_nodes(node, graph)
+        visited_this_iter = set()
+    print(f'Topological sorting is {tp_sorted_nodes}')
+
+topological_sort(other_graph)
